1e32405ecae2d60910e1c780bc39c5b9c8f6b26c
zhouyi <kkakoz@163.com>
2023-01-23 21:27:45 +0800

    fix:redis

--- redisx/redis_lock.go
+++ redisx/redis_lock.go
@@ -1,17 +1,18 @@
 package redisx
 
 import (
+	"context"
 	uuid "github.com/satori/go.uuid"
 	"time"
 )
 
 type unLockFunc func()
 
-func Lock(key string, timeout time.Duration) (bool, unLockFunc, error) {
+func Lock(ctx context.Context, key string, timeout time.Duration) (bool, unLockFunc, error) {
 	value := uuid.NewV4().String()
-	result, err := client.SetNX(key, value, timeout).Result()
+	result, err := client.SetNX(ctx, key, value, timeout).Result()
 	unLock := func() {
-		_, err = client.Eval("if redis.call('get',KEYS[1]) == ARGV[1] then"+
+		_, err = client.Eval(ctx, "if redis.call('get',KEYS[1]) == ARGV[1] then"+
 			"   return redis.call('del',KEYS[1]) "+
 			"else"+
 			"   return 0 "+
